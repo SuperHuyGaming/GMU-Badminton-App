404237038eb96d79cf4c4e4b4c13bf3fa4dc772a
SuperHuyGaming <SuperHuyGaming@users.noreply.github.com>
2026-09-22 22:58:12 -0400

    fix(ui): prevent Tournaments tab from being fully uppercase

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -155,7 +155,11 @@ export default function Navbar() {
 									color={location.pathname === "/tournaments" ? "secondary" : "inherit"}
 									component={RouterLink}
 									to="/tournaments"
-									sx={{ mx: 1, fontWeight: "bold" }}
+									sx={{ 
+										mx: 1, 
+										fontWeight: location.pathname === "/tournaments" ? 800 : 600,
+										textTransform: 'none'
+									}}
 								>
 									Tournaments
 								</Button>
